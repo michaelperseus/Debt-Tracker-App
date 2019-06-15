69622cd3b8f7a6f617fa0b6ce8b76b2fb2bc80a7
Michael Boro <michaelboro1122@gmail.com>
2019-06-14 21:27:23 -0400

    added hbs support for front end

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
+//Loads in NPM Modules that will be used
+const path = require('path');
+const hbs = require('hbs');
 const express = require('express');
 const mongoose = require('mongoose');
+
+//Loads in Mongoose Models that will be used
 const Debt = require('./models/debt');
 
+//Loads the mongoDB connection
 require('./db/mongoose.js');
 
-
+//Initializes the express server variable
 const app = express();
-
 app.use(express.json());
+const port = process.allowedNodeEnvironmentFlags.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Welcome to the page!")
-});
+//Defines path for express to configure hbs
+const publicDirectoryPath = path.join(__dirname,"../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
+//Further setup for hbs, pointing to views and partials location on server
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Renders the main page
+app.get('', (req, res) => {
+    res.render('index', {
+        name: 'Michael Boro'
+    })
+})
+
+
+//Adds a new debt to the database with sent data
 app.post("/newDebt", async (req, res) => {
     console.log(req.body);
     const debt = new Debt(req.body);
@@ -25,6 +46,21 @@ app.post("/newDebt", async (req, res) => {
     
 })
 
-app.listen(3000, () => {
-    console.log("App launched on port 3000");
+//Returns the requested Debt and displays the remaining balance
+app.get("/seeDebt/:id", async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const debt = await Debt.findById(_id);
+        const remaining = (debt.startingBalance - debt.amountPaid);
+        res.send(`You have ${remaining} left to pay!`);
+    } catch (e) {
+        res.send(e);
+    }
+})
+
+
+//Starts the server on the proper port
+app.listen(port, () => {
+    console.log(`Sever launched on port ${port}`);
 })
